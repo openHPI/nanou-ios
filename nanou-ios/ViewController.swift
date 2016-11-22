aaaa1e609c43f947732bad01ce187947ccb67950
Max Bothe <mathebox@gmail.com>
2016-11-22 09:53:36 +0100

    Initial Commit

---
+++ nanou-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nanou-ios
+//
+//  Created by Max Bothe on 22/11/16.
+//  Copyright © 2016 Max Bothe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
