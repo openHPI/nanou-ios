a53989775ec3ef70410033da221da9eff544e01c
Max Bothe <mathebox@gmail.com>
2016-12-05 11:23:55 +0100

    feat(styling): set global tint color

--- nanou-ios/AppDelegate.swift
+++ nanou-ios/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UITabBar.appearance().tintColor = UIColor.nanouOrange
 
         // Configure CocoaLumberJack
         DDLog.add(DDTTYLogger.sharedInstance())
