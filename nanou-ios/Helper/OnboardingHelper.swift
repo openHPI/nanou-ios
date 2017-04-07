822134889a18fdcfd3292115b48c14b877c9447e
Max Bothe <mathebox@gmail.com>
2017-04-07 10:34:40 +0200

    fix(ux): reword onboarding
    - add screens for login

--- nanou-ios/Helper/OnboardingHelper.swift
+++ nanou-ios/Helper/OnboardingHelper.swift
@@ -48,27 +48,33 @@ struct OnboardingHelper {
         let page0 = NanouOnboardingContentViewController(body: "Wie funktioniert nanou?", image: UIImage(named: "OnboardingPage0")!, buttonText: "Weiter", action: nil)
         page0.movesToNextViewController = true
 
-        let page1 = NanouOnboardingContentViewController(body: "Interessen wählen.", image: UIImage(named: "OnboardingPage1")!, buttonText: "Weiter", action: nil)
+        let page1 = NanouOnboardingContentViewController(body: "Wähle deine Interessen.", image: UIImage(named: "OnboardingPage1")!, buttonText: "Weiter", action: nil)
         page1.movesToNextViewController = true
 
         let page2 = NanouOnboardingContentViewController(body: "Ein Video wird vorgeschlagen.", image: UIImage(named: "OnboardingPage2")!, buttonText: "Weiter", action: nil)
         page2.movesToNextViewController = true
 
-        let page3 = NanouOnboardingContentViewController(body: "Video auswählen, wenn es gefällt.", image: UIImage(named: "OnboardingPage3")!, buttonText: "Weiter", action: nil)
+        let page3 = NanouOnboardingContentViewController(body: "Wähle das Video aus, wenn es dich anspricht.", image: UIImage(named: "OnboardingPage3")!, buttonText: "Weiter", action: nil)
         page3.movesToNextViewController = true
 
-        let page4 = NanouOnboardingContentViewController(body: "Video ansehen und bewerten. Das verbessert die nächsten Vorschläge.", image: UIImage(named: "OnboardingPage5")!, buttonText: "Weiter", action: nil)
+        let page4 = NanouOnboardingContentViewController(body: "Bewerte das Video nach dem Anschauen. Das verbessert die nächsten Vorschläge.", image: UIImage(named: "OnboardingPage5")!, buttonText: "Weiter", action: nil)
         page4.movesToNextViewController = true
 
         let page5 = NanouOnboardingContentViewController(body: "Ein neues Video wird freigeschaltet, da die Videos aufeinander aufbauen.", image: UIImage(named: "OnboardingPage2")!, buttonText: "Weiter", action: nil)
         page5.movesToNextViewController = true
 
-        let page6 = NanouOnboardingContentViewController(body: "Falls das Video nicht gefällt, einfach wegklicken. Es wird später wieder vorgeschlagen.", image: UIImage(named: "OnboardingPage4")!, buttonText: "Fertig", action: {
+        let page6 = NanouOnboardingContentViewController(body: "Wenn dich das Video nicht anspricht, klicke es einfach weg. Es wird später wieder vorgeschlagen.", image: UIImage(named: "OnboardingPage4")!, buttonText: "Weiter", action: nil)
+        page6.movesToNextViewController = true
+
+        let page7 = NanouOnboardingContentViewController(body: "Du kannst nanou ohne Einschränkungen testen. Dein Fortschritt bleibt in der App gespeichert.", image: UIImage(named: "OnboardingPage6")!, buttonText: "Weiter", action: nil)
+        page7.movesToNextViewController = true
+
+        let page8 = NanouOnboardingContentViewController(body: "Wenn du deinen Fortschritt auf mehreren Geräten nutzen möchtest, verwende den Drittanbieter-Login. Das funktioniert auch nachträglich.", image: UIImage(named: "OnboardingPage7")!, buttonText: "Fertig", action: {
             OnboardingHelper.onboardingViewController?.dismiss(animated: true, completion: nil)
         })
 
         let backgroundImage = UIImage.onboardingGradient(frame: viewController.view.frame)!
-        let onboardingViewController = NanouOnboardingViewController(backgroundImage: backgroundImage, contents: [page0, page1, page2, page3, page4, page5, page6])!
+        let onboardingViewController = NanouOnboardingViewController(backgroundImage: backgroundImage, contents: [page0, page1, page2, page3, page4, page5, page6, page7, page8])!
         onboardingViewController.shouldFadeTransitions = true
         onboardingViewController.fadePageControlOnLastPage = true
         onboardingViewController.fadeSkipButtonOnLastPage = true
